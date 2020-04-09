bffef83a3ae3a49d480f2df8b3038f265af71b9e
HarryChiu <harrychiu1106@gmail.com>
2020-04-09 17:25:10 +0800

    Updates

---
+++ precache-manifest.78c891d15d64ede9e1a447a7b8aea409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe75f6fcdf48e08d93ff5f3e1ba69849",
+    "url": "/border-radius-previewer/index.html"
+  },
+  {
+    "revision": "1cfbe1d2ca29ac2f2782",
+    "url": "/border-radius-previewer/static/css/main.7a1fff46.chunk.css"
+  },
+  {
+    "revision": "799d882719148cb80170",
+    "url": "/border-radius-previewer/static/js/2.9e78a148.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/border-radius-previewer/static/js/2.9e78a148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cfbe1d2ca29ac2f2782",
+    "url": "/border-radius-previewer/static/js/main.93701557.chunk.js"
+  },
+  {
+    "revision": "ad5453d21e4e05b10c88",
+    "url": "/border-radius-previewer/static/js/runtime-main.28dd0dae.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/border-radius-previewer/precache-manifest.78c891d15d64ede9e1a447a7b8aea409.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/border-radius-previewer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
